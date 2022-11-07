d49690345a0ee4c557b02380a75a8bb6718dece0
Vishnu Pillai <65006856+vishnupillai704@users.noreply.github.com>
2022-11-07 19:32:25 +0530

    Update sandeep.groovy

--- vars/sandeep.groovy
+++ vars/sandeep.groovy
@@ -15,7 +15,7 @@ def call() {
         stage('terraform '){
         steps{
             
-            sh "git clone  "https://github.com/singusandeep/terra.git
+            sh "git clone  https://github.com/singusandeep/terra.git"
         
         }
             
