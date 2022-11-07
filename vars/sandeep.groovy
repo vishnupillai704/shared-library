7a2a0d5ec05b38b5df228f55de7e5c30a4605234
Vishnu Pillai <65006856+vishnupillai704@users.noreply.github.com>
2022-11-07 19:28:54 +0530

    Update sandeep.groovy

--- vars/sandeep.groovy
+++ vars/sandeep.groovy
@@ -33,7 +33,7 @@ def call() {
         stage('terraform apply') {
         steps {
                
-            withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws-jenkins', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]){
+            withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'AWS-sandeep', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]){
             sh 'aws --version'
             
             sh 'terraform apply -auto-approve -var AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -var AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY'
