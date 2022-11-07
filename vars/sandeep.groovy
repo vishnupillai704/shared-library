56b80d6abd72a807ff9b71c90e478bb810ddee2d
Vishnu Pillai <65006856+vishnupillai704@users.noreply.github.com>
2022-11-07 19:23:10 +0530

    Update sandeep.groovy

--- vars/sandeep.groovy
+++ vars/sandeep.groovy
@@ -1,1 +1,49 @@
+def call() {
+  
+  pipeline {
+    agent any
 
+
+
+  tools {
+     terraform 'terraform'
+    }
+
+
+
+   stages {
+        stage('terraform '){
+        steps{
+            
+            sh "git clone  "https://github.com/singusandeep/terra.git
+        
+        }
+            
+        }
+
+
+
+           stage('terraform init') {
+        steps {   
+                                                        
+            sh 'terraform init'
+          
+         }
+    }
+        stage('terraform apply') {
+        steps {
+               
+            withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws-jenkins', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]){
+            sh 'aws --version'
+            
+            sh 'terraform apply -auto-approve -var AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -var AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY'
+            
+            
+            }
+        }
+    }  
+        
+    }
+}
+  
+}
