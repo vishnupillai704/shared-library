9c86098f404108131e3abe0dac8bd52bb6655da8
Vishnu Pillai <65006856+vishnupillai704@users.noreply.github.com>
2022-11-07 12:09:58 +0530

    Update terraform.groovy

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -19,21 +19,21 @@ def call() {
 
             stage('terraform init') {
         steps {   
-                                                                  
+          dir(C:\Users\VIPILLAI\.jenkins\workspace\shared\terraform\){                                                
             sh 'terraform init'
-            
+          }
          }
     }
         stage('terraform apply') {
         steps {
-             
+             dir(C:\Users\VIPILLAI\.jenkins\workspace\shared\terraform\){  
             withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws-jenkins', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]){
             sh 'aws --version'
             
             sh 'terraform apply -auto-approve -var AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -var AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY'
             sh 'aws --version'
             sh 'terraform apply -auto-approve'
-            
+            }
             
             }
         }
