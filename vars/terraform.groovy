1adbb36a0de8eee9efc62c33f379ed714693fd2d
Vishnu Pillai <65006856+vishnupillai704@users.noreply.github.com>
2022-11-07 12:10:53 +0530

    Update terraform.groovy

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -11,7 +11,7 @@ def call() {
         stage('terraform '){
         steps{
             
-            sh "git clone https://github.com/vishnupillai704/terraform/ "
+            sh "git pull https://github.com/vishnupillai704/terraform/ "
         
         }
             
