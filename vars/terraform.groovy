1abe2bbcc5b70db421fb074b439e5507e1bbcf9e
Vishnu Pillai <65006856+vishnupillai704@users.noreply.github.com>
2022-11-07 10:19:30 +0530

    Update terraform.groovy

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,46 +1,48 @@
 def call(){
-  pipeline {
-    agent any
+  sh "echo hello"
+  
+//   pipeline {
+//     agent any
 
-   tools {
-     terraform 'terraform'
-    }
+//    tools {
+//      terraform 'terraform'
+//     }
 
-    stages {
-        stage('terraform '){
-        steps{
-            dir('/var/lib/jenkins/workspace/terra/terraform') {
-            sh "git pull https://github.com/vishnupillai704/terraform/ "
-        }
-        }
+//     stages {
+//         stage('terraform '){
+//         steps{
+//             dir('/var/lib/jenkins/workspace/terra/terraform') {
+//             sh "git pull https://github.com/vishnupillai704/terraform/ "
+//         }
+//         }
             
-        }
+//         }
 
-            stage('terraform init') {
-        steps {
-            dir('/var/lib/jenkins/workspace/terra/terraform') {
+//             stage('terraform init') {
+//         steps {
+//             dir('/var/lib/jenkins/workspace/terra/terraform') {
             
             
-            sh 'terraform init'
-            }
-         }
-    }
-        stage('terraform apply') {
-        steps {
-            dir('/var/lib/jenkins/workspace/terra/terraform') {
-            withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws-jenkins', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]){
-            sh 'aws --version'
+//             sh 'terraform init'
+//             }
+//          }
+//     }
+//         stage('terraform apply') {
+//         steps {
+//             dir('/var/lib/jenkins/workspace/terra/terraform') {
+//             withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws-jenkins', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]){
+//             sh 'aws --version'
             
-            sh 'terraform apply -auto-approve -var AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -var AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY'
-            sh 'aws --version'
-            sh 'terraform apply -auto-approve'
-            }
+//             sh 'terraform apply -auto-approve -var AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -var AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY'
+//             sh 'aws --version'
+//             sh 'terraform apply -auto-approve'
+//             }
             
-            }
-        }
-    }  
+//             }
+//         }
+//     }  
         
-    } 
-}
+//     } 
+// }
   
 }
