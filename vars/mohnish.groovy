cb85f7873672211a022b637256c6ea489dc34fc9
Vishnu Pillai <65006856+vishnupillai704@users.noreply.github.com>
2022-11-08 11:21:45 +0530

    Update mohnish.groovy

--- vars/mohnish.groovy
+++ vars/mohnish.groovy
@@ -12,10 +12,10 @@ pipeline {
    stages {
         stage('build'){
             steps{
-                dir('/var/lib/jenkins/workspace/mohnish_terraform/terraform'){
+                
                 sh "git pull https://github.com/MohnishBhonde/terraform"
                 
-            }
+            
         }
     }
 
@@ -23,22 +23,22 @@ pipeline {
 
            stage('terraform init') {
         steps {
-            dir('/var/lib/jenkins/workspace/mohnish_terraform/terraform') {
+          //dir('/var/lib/jenkins/workspace/mohnish_terraform/terraform') {
             
             
             sh 'terraform init'
-            }
+          // }
          }
     }
         stage('terraform apply') {
         steps {
-            dir('/var/lib/jenkins/workspace/mohnish_terraform/terraform') {
+           //dir('/var/lib/jenkins/workspace/mohnish_terraform/terraform') {
             withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws_mohnish', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]){
             sh 'aws --version'
             
             sh 'terraform apply -auto-approve -var AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -var AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY'
            
-            }
+            //}
             
             }
         }
