7406e19be47892d840636f598d033d24ca27a2cf
Vishnu Pillai <65006856+vishnupillai704@users.noreply.github.com>
2022-11-08 17:48:36 +0530

    Create deploy.groovy

---
+++ com/mcd/deploy.groovy
@@ -0,0 +1,44 @@
+package com.mcd
+def call() {
+  
+  pipeline {
+    agent any
+
+   tools {
+     terraform 'terraform'
+    }
+
+    stages {
+        stage('terraform '){
+        steps{
+            
+            sh "git clone https://github.com/vishnupillai704/terraform/ "
+        
+        }
+            
+        }
+
+            stage('terraform init') {
+        steps {   
+                                                        
+            sh 'terraform init'
+          
+         }
+    }
+        stage('terraform apply') {
+        steps {
+               
+            withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws-jenkins', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]){
+            sh 'aws --version'
+            
+            sh 'terraform apply -auto-approve -var AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -var AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY'
+            
+            
+            }
+        }
+    }  
+        
+    } 
+}
+  
+}
