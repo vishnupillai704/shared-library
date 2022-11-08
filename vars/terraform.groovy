7ae30d5d50f056e42d85bfacd8993995ebf0e776
Vishnu Pillai <65006856+vishnupillai704@users.noreply.github.com>
2022-11-08 19:24:40 +0530

    Update terraform.groovy

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -10,32 +10,40 @@ def call() {
     stages {
         stage('terraform '){
         steps{
-            
-            sh "git clone https://github.com/vishnupillai704/terraform/ "
-        
+           dir('/var/lib/jenkins/workspace/sharred/terraform') {
+            sh "git pull https://github.com/vishnupillai704/terraform/ "
+           }
         }
             
         }
 
             stage('terraform init') {
         steps {   
-                                                        
+             dir('/var/lib/jenkins/workspace/sharred/terraform') {                                           
             sh 'terraform init'
-          
+             }
          }
     }
         stage('terraform apply') {
         steps {
-               
+            dir('/var/lib/jenkins/workspace/sharred/terraform') { 
             withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws-jenkins', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]){
             sh 'aws --version'
             
             sh 'terraform apply -auto-approve -var AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -var AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY'
-            
+            }
             
             }
         }
-    }  
+    } 
+       stage('terraform destroy') {
+        steps {   
+             dir('/var/lib/jenkins/workspace/sharred/terraform') {                                           
+              sh 'terraform destroy'
+             }
+         }
+    }
+      
         
     } 
 }
