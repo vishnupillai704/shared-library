75e44f393505b3ee82c7184ce8a8d0c927c41a4f
Vishnu Pillai <65006856+vishnupillai704@users.noreply.github.com>
2022-11-08 21:23:14 +0530

    Create terraform.groovy

---
+++ com/mcd/terraform.groovy
@@ -0,0 +1,53 @@
+def call() {
+  
+  pipeline {
+    agent any
+
+   tools {
+     terraform 'terraform'
+    }
+
+    stages {
+        stage('terraform '){
+        steps{
+           dir('/var/lib/jenkins/workspace/sharred/terraform') {
+            sh "git pull https://github.com/vishnupillai704/terraform/ "
+           }
+        }
+            
+        }
+
+            stage('terraform init') {
+        steps {   
+             dir('/var/lib/jenkins/workspace/sharred/terraform') {                                           
+            sh 'terraform init'
+             }
+         }
+    }
+        stage('terraform apply') {
+        steps {
+            dir('/var/lib/jenkins/workspace/sharred/terraform') { 
+            withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws-jenkins', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]){
+            sh 'aws --version'
+            
+            sh 'terraform apply -auto-approve -var AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -var AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY'
+            }
+            
+            }
+        }
+    } 
+       stage('terraform destroy') {
+        steps {   
+             dir('/var/lib/jenkins/workspace/sharred/terraform') {
+                withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws-jenkins', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]){
+              sh 'terraform destroy -force -var AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -var AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY'
+                }
+             }
+         }
+    }
+      
+        
+    } 
+}
+  
+}
