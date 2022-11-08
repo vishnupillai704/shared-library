698e305f5be0ae6cfb65953a22bfe1b4e42586b7
Vishnu Pillai <65006856+vishnupillai704@users.noreply.github.com>
2022-11-08 20:11:36 +0530

    Update snsEc2Terra.groovy

--- com/mcd/snsEc2Terra.groovy
+++ com/mcd/snsEc2Terra.groovy
@@ -14,10 +14,10 @@ pipeline {
    stages {
         stage('build'){
             steps{
-                dir('/var/lib/jenkins/workspace/shared_ec2Terra/terraform'){
-                sh "git pull https://github.com/MohnishBhonde/terraform"
+               //dir('/var/lib/jenkins/workspace/shared_ec2Terra/terraform'){
+                sh "git clone https://github.com/MohnishBhonde/terraform"
                 
-                }
+                //}
         }
     }
 
@@ -25,22 +25,22 @@ pipeline {
 
            stage('terraform init') {
         steps {
-          dir('/var/lib/jenkins/workspace/shared_ec2Terra/terraform') {
+          //dir('/var/lib/jenkins/workspace/shared_ec2Terra/terraform') {
             
             
             sh 'terraform init'
-           }
+           //}
          }
     }
         stage('terraform apply') {
         steps {
-           dir('/var/lib/jenkins/workspace/shared_ec2Terra/terraform') {
+           //dir('/var/lib/jenkins/workspace/shared_ec2Terra/terraform') {
             withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws_mohnish', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]){
             sh 'aws --version'
             
             sh 'terraform apply -auto-approve -var AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -var AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY'
            
-            }
+            //}
             
             }
         }
