f669cb51c035be9cdea4fb85d2ff04e36d0f08ce
Vishnu Pillai <65006856+vishnupillai704@users.noreply.github.com>
2022-11-09 15:16:23 +0530

    Update artifactory.groovy

--- vars/artifactory.groovy
+++ vars/artifactory.groovy
@@ -1,1 +1,79 @@
+ def call() {
+    
+  pipeline {
+     agent any
 
+    tools {
+        maven "maven1" // You need to add a maven with name "3.6.0" in the Global Tools Configuration page
+    }
+
+    stages {
+        stage("Build") {
+            steps {
+                
+                
+                sh "git clone https://github.com/singusandeep/devo"
+                
+               
+                
+                
+                
+            }
+         }
+        stage("mvn install"){
+            steps{
+                 dir('/var/lib/jenkins/workspace/shared_librari_sandeep_ci/devo'){
+                  sh "mvn -version"
+                  sh "mvn clean install"
+                 }
+            }
+        }
+        
+            stage("mvn package"){
+                steps{
+                    dir('/var/lib/jenkins/workspace/shared_librari_sandeep_ci/devo'){
+                    sh "mvn package"
+                    }
+                }
+            }
+            stage("server"){
+                steps{
+                    rtServer (
+                        id: "artifactory-server",
+                        url:'http://ec2-52-25-243-112.us-west-2.compute.amazonaws.com:8081/artifactory',
+                        username:'jenkins',
+                        password: "jenkins@123", 
+                        bypassProxy: true,
+                        timeout:300,
+                        )
+                    
+                }
+                
+            }
+            stage('Upload'){
+                steps{
+                    rtUpload(
+                        serverId:"artifactory-server",
+                        spec:'''{
+                            "files":[
+                            {
+                            "pattern":"*.jar",
+                            "target":"generic-local"
+                            }]
+                        }''',
+                        
+                        )
+                }
+            }
+            stage('Publish build info'){
+                steps{
+                    rtPublishBuildInfo(
+                        serverId:"artifactory-server"
+                        )
+                }
+            }
+           
+    
+        }
+    }
+}
